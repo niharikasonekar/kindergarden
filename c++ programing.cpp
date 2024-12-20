dd958e40e463df4b1edd9ab0389073ff8ecda734
niharikasonekar <niharika151196@gmil.com>
2024-12-20 17:35:34 +0530

    array

--- c++ programing.cpp
+++ c++ programing.cpp
@@ -2,10 +2,22 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[]={1,2,3,4,5};
-    
-    for(int i=0;i<5;i++){
-    cout<<arr[i];
+int arr[]={1,2,3,2,1};
+for(int i=0;i<5;++i)
+{
+    bool dup=false;
+    for(int j=i+1; j<5; ++j)
+    {
+        if(arr[i]==arr[j] && arr[i]!=-1)
+        {
+            arr[j]=-1;
+            dup=true;
+            
+        }
+    }
+    if(dup)
+    {
+        cout<<arr[i];
+    }
 }
-    return 0;
 }
